e2b3a278c3b3497489f04c5badd73d60872aee86
enesscevik <enesc.1905.759@gmail.com>
2024-08-18 20:20:18 +0300

    auto scan  and calculation with range

--- content.js
+++ content.js
@@ -1,13 +1,14 @@
-function calculator() {
+async function calculator(rangeBegin, range) {
   const here = window.location.href;
   if (!here.includes('playlist')) {
-    return `There is no playlist!`;
+    return 'no';
   }
-
-  timeElements = document.getElementsByClassName('badge-shape-wiz__text');
-
-  let targetTimeElements = Array.from(timeElements).filter(isElementVisible);
-
+  let targetTimeElements = visibleElementsFinder('badge-shape-wiz__text');
+  if (targetTimeElements.length != indexFinder()) {
+    await uploadThemAsync();
+    targetTimeElements = visibleElementsFinder('badge-shape-wiz__text');
+  }
+  targetTimeElements = targetTimeElements.splice(rangeBegin, range);
   let totalSeconds = 0;
   targetTimeElements.forEach((el) => {
     let timeText = el.textContent.trim();
@@ -19,10 +20,16 @@ function calculator() {
       totalSeconds += timeParts[0] * 60 + timeParts[1];
     }
   });
-
-  return `${
-    targetTimeElements.length
-  } videos were included in the calculation! <br>
+  console.log(targetTimeElements.length);
+  const info =
+    targetTimeElements.length == indexFinder()
+      ? `<strong>${
+          targetTimeElements.length
+        }</strong>/<strong>${indexFinder()}</strong>`
+      : `(<strong>${rangeBegin + 1}</strong> - <strong>${
+          rangeBegin + range
+        }</strong>)`;
+  return `<p>${info} videos were included in the calculation!</p>
   <table id="resultTable"> 
     ${convertToTimeFormat(totalSeconds, 1)} ${convertToTimeFormat(
     totalSeconds,
@@ -32,6 +39,21 @@ function calculator() {
     1.75
   )} ${convertToTimeFormat(totalSeconds, 2)}
   </table>`;
+}
+
+function visibleElementsFinder(cl) {
+  vElements = document.getElementsByClassName(cl);
+  return Array.from(vElements).filter(isElementVisible);
+}
+
+function indexFinder() {
+  const a = visibleElementsFinder('style-scope yt-formatted-string');
+  let pre = '';
+  for (let i = 0; i < a.length; i++) {
+    if (a[i].textContent.includes('video')) break;
+    else pre = a[i].textContent;
+  }
+  return pre;
 }
 
 function isElementVisible(el) {
@@ -56,7 +78,97 @@ function convertToTimeFormat(total, speed) {
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'scrape') {
-    const data = calculator();
-    sendResponse({ data: data });
+    (async () => {
+      const i = Number(indexFinder());
+      const begin = message.begin <= i ? message.begin : 1;
+      const end = message.end <= i ? message.end : i;
+
+      const data = await calculator(begin - 1, end - begin + 1);
+
+      sendResponse({ data: data });
+    })();
+    return true;
+  }
+  if (message.action === 'scrollT') {
+    if (
+      document.documentElement.scrollHeight - window.scrollY >=
+      window.scrollY
+    )
+      window.scrollTo(0, document.documentElement.scrollHeight);
+    else window.scrollTo(0, 0);
   }
 });
+
+function scrollPage() {
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      window.scrollBy(0, window.innerHeight);
+      if (
+        document.documentElement.scrollHeight <=
+        window.scrollY + window.innerHeight
+      ) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 5);
+  });
+}
+// async function uploadThemv2() {
+//   const y = window.scrollY;
+//   let s = -200;
+//   window.scrollTo(0, 0);
+//   while (true) {
+//     const uploaded = visibleElementsFinder('badge-shape-wiz__text').length;
+//     if (uploaded == totalVideos) break;
+//     s += 200;
+//     window.scrollTo(0, s);
+//     await new Promise((resolve) => setTimeout(resolve, 5)); // 1 saniye bekleme
+//     console.log(uploaded + ' != ' + totalVideos);
+//   }
+//   window.scrollTo(0, y);
+// }
+
+async function uploadThemAsync() {
+  const y = window.scrollY;
+  let i = 0;
+
+  const scrollInterval = () => {
+    return new Promise(async (resolve) => {
+      const scrollStep = async () => {
+        window.scrollTo(0, i);
+        i += 200;
+
+        if (i >= document.documentElement.scrollHeight) {
+          window.scrollTo(0, y);
+          resolve();
+          return;
+        }
+
+        await new Promise((res) => setTimeout(res, 5));
+        await scrollStep(); // Bir sonraki adıma geç
+      };
+
+      await scrollStep(); // İlk kaydırma adımını başlat
+    });
+  };
+
+  window.scrollTo(0, document.documentElement.scrollHeight);
+  await new Promise((resolve) => setTimeout(resolve, 250)); // Sayfanın yüklenmesini bekleyin
+  await scrollInterval(); // Sayfanın sonuna kadar kaydırmayı başlatın
+}
+
+// function uploadThem() {
+//   const y = window.scrollY;
+//   let i = 0;
+//   const scrollInterval = () => {
+//     window.scrollTo(0, i);
+//     i += 200;
+//     if (i >= document.documentElement.scrollHeight) {
+//       window.scrollTo(0, y);
+//       return;
+//     }
+//     setTimeout(scrollInterval, 5);
+//   };
+//   window.scrollTo(0, document.documentElement.scrollHeight);
+//   setTimeout(scrollInterval, 250);
+// }
--- popup.js
+++ popup.js
@@ -1,11 +1,81 @@
-document.getElementById('calcButton').addEventListener('click', () => {
+document.getElementById('calculate').addEventListener('click', () => {
   const res = document.getElementById('result');
+  const cal = document.getElementById('calculator');
+  cal.style.display = 'none';
+  res.style.display = 'flex';
   res.innerHTML = '';
   res.classList.add('loading');
+  const b = document.getElementById('beginId').value;
+  const begin = b ? (b > 0 ? Number(b) : 1) : 1;
+  const e = document.getElementById('endId').value;
+  const end = e ? (Number(e) >= Number(begin) ? Number(e) : 999) : 999;
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'scrape' }, (response) => {
-      res.classList.remove('loading');
-      res.innerHTML = response.data;
-    });
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { action: 'scrape', begin: begin, end: end },
+      (response) => {
+        res.classList.remove('loading');
+        if (response.data == 'no') {
+          res.innerHTML = `<p style="transform: translateY(10px)"><b>There is no playlist!</b></p>`;
+          setTimeout(() => {
+            res.style.display = 'none';
+            cal.style.display = 'unset';
+          }, 1500);
+        } else res.innerHTML = response.data;
+      }
+    );
   });
+});
+
+document.getElementById('scrollTop').addEventListener('click', () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { action: 'scrollT' },
+      (response) => {}
+    );
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.getElementsByClassName('calcButton');
+
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    const tooltip = button.querySelector('.tooltip');
+
+    let hovertimer;
+    button.addEventListener('mouseover', (event) => {
+      hovertimer = setTimeout(() => {
+        tooltip.textContent = button.getAttribute('data-tooltip');
+        tooltip.style.display = 'block';
+        tooltip.style.left = `${event.pageX + 10}px`; // Sağ tarafta biraz boşluk bırak
+        tooltip.style.top = `${event.pageY + 10}px`; // Alt tarafta biraz boşluk bırak
+      }, 1000);
+    });
+
+    button.addEventListener('mouseout', () => {
+      clearTimeout(hovertimer);
+      tooltip.style.display = 'none';
+    });
+
+    button.addEventListener('click', () => {
+      clearTimeout(hovertimer);
+      tooltip.style.display = 'none';
+    });
+  }
+});
+
+document.getElementById('calTab').addEventListener('click', () => {
+  document.getElementById('result').style.display = 'none';
+  document.getElementById('calculator').style.display = 'unset';
+});
+
+setTimeout(() => {
+  document.getElementById('calculator').style.display = 'unset';
+}, 1000);
+
+document.getElementById('all').addEventListener('click', () => {
+  document.getElementById('beginId').value = '0';
+  document.getElementById('endId').value = '999';
 });
